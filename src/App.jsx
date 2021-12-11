e43a0f2f3ffc5938784d66450c099e3b7ec2df0d
alv2r <alv.rsl@gmail.com>
2021-12-11 16:11:35 -0500

    Adds react-markdown library and custom styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
+import { useState } from "react";
+import ReactMarkdown from "react-markdown";
+// import remarkPresetWooorm from "remark-preset-wooorm";
+
 function App() {
+  const [markdown, setMarkdown] = useState();
+
+  function inputHandler(e) {
+    setMarkdown(e.target.value);
+  }
+
   return (
     <div className="grid min-h-screen grid-cols-2 sm:flex-row">
-      <textarea className="p-10 font-mono text-lg text-gray-600 shadow-md outline-none" />
-      <div className="px-10 bg-gray-50 py-14"></div>
+      <textarea
+        className="p-10 font-mono text-lg text-gray-600 shadow-md outline-none"
+        onChange={inputHandler}
+      />
+      <div className="px-10 text-gray-800 bg-gray-50 py-14">
+        <ReactMarkdown children={markdown} />
+      </div>
     </div>
   );
 }
