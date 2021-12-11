1edc77efb8018208875c933775d26d6bd9423c85
alv2r <alv.rsl@gmail.com>
2021-12-10 22:23:23 -0500

    Initial commit

---
+++ craco.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+
+module.exports = {
+  webpack: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@components": path.resolve(__dirname, "src/components"),
+    },
+    // configure: {
+    //   module: {
+    //     rules: [
+    //       {
+    //         type: "javascript/auto",
+    //         test: /\.mjs$/,
+    //         include: /node_modules/,
+    //       },
+    //     ],
+    //   },
+    // },
+  },
+  style: {
+    postcss: {
+      plugins: [require("tailwindcss"), require("autoprefixer")],
+    },
+  },
+};
---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="grid min-h-screen grid-cols-2 sm:flex-row">
+      <textarea className="p-10 font-mono text-lg text-gray-600 shadow-md outline-none" />
+      <div className="px-10 bg-gray-50 py-14"></div>
+    </div>
+  );
+}
+
+export default App;
